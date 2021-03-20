b88fc42bcf3be10cf791ae9078c9f81f170e0679
Kanishk Bharadwaj <kanishkbharadwaj333@gmail.com>
2021-03-20 23:19:56 +0530

    added bg img to index file (ny img tag)

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 import HeaderIndex from './Components/HeaderIndex';
+import SectionIndex from './Components/SectionIndex';
 
 function App() {
   return (
+    <>
     <HeaderIndex 
     logo="/assets/test.png" 
     option1="Home" 
     option2="Option 2" 
     option3="Option 3" 
-    option4="Option 4" 
-    option5="Option 5" />
+    option4="Sign Up" 
+    option5="Log in" />
+
+    <SectionIndex/>
+    </>
   );
 }
 
---
+++ src/Components/SectionIndex.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../Component-Styles/SectionIndex.css';
+
+
+function SectionIndex(){
+    return(
+        <>
+        <div className="section">
+            <img src="/assets/indexBackground.jpg"/>
+        </div>
+        </>
+    )
+}
+
+export default SectionIndex;

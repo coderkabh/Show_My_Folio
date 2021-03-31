dc38bcfeea662b5554bf0c878a61babaeccf8e29
Kanishk Bharadwaj <kanishkbharadwaj333@gmail.com>
2021-03-31 14:11:27 +0530

    added sign in component

--- src/Components/SignIn.jsx
+++ src/Components/SignIn.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Route, Switch} from 'react-router-dom';
 
 function SignIn() {
     return(
         <>
-
+        <h1>This is Sign in </h1>
         </>
     );
 }

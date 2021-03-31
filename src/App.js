2f12aa773df6430bf3e10ba4a068a4fdc8cf5bdf
Kanishk Bharadwaj <kanishkbharadwaj333@gmail.com>
2021-03-31 14:12:35 +0530

    Routed URLs for sign-in and / page

--- src/App.js
+++ src/App.js
@@ -3,20 +3,17 @@ import ReactDOM from "react-dom";
 import HeaderIndex from './Components/HeaderIndex';
 import SectionIndex from './Components/SectionIndex';
 import pic from "../src/assets/portfolio.jpg";
-
+import SignIn from './Components/SignIn';
+import Home from './Components/Home';
+import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <>
-    <HeaderIndex 
-    logo = {pic}
-    option1="Home" 
-    option2="Option 2" 
-    option3="Option 3" 
-    option4="Sign Up" 
-    option5="Log in" />
-
-    <SectionIndex/>
+    <Switch>
+      <Route exact path="/" component = {Home} />
+      <Route exact path="/sign-in" component = {SignIn}/>
+    </Switch>
     </>
   );
 }

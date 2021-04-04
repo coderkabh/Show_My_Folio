49a06cf0a35112c90102c1dea13635839cfb55f8
Kanishk Bharadwaj <kanishkbharadwaj333@gmail.com>
2021-04-04 17:44:15 +0530

    made component SignUp

---
+++ src/Components/SignUp.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import "../component-Styles/SignUp.css";
+import HeaderIndex from './HeaderIndex';
+import pic from '../assets/portfolio.jpg';
+import { Form } from 'react-bootstrap';
+
+function SignUp() {
+    return(
+        <>
+        <HeaderIndex 
+        logo = {pic}
+        option1="Home" 
+        link1 = "/"
+        option2="About Us" 
+        link2 = "/about-us"
+        option3="Log In" 
+        link3 = "/log-in"
+        option4="Sign Up"
+        link4 = "#" 
+        option5="Contact Us"
+        link5 = "/contact-us" />
+
+        {/* From here Form starts */}
+
+
+        <div className="main-div">
+            <div className="box">
+                <h1>SHOW MY FOLIO</h1>
+                <Form method="" action="">
+
+                    <div className="input-box">
+                        <input type="text" name="username" autoComplete="off" required />
+                        <label> Username </label>
+                    </div>
+
+                    <div className="input-box">
+                        <input type="password" name="password" autoComplete="off" required />
+                        <label> Password </label>
+                    </div>
+
+                    <input type="submit" name="" value="Log In"/>
+
+                </Form>
+            </div>
+        </div>
+        
+        </>
+    );
+}
+
+export default SignUp;

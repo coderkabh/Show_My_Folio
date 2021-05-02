d0f5cb1b452cbac6e874690e5b66b25673498809
Kanishk Bharadwaj <kanishkbharadwaj333@gmail.com>
2021-05-02 23:32:12 +0530

    Header Complete

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import HeaderIndex from './HeaderIndex';
+import pic from '../assets/portfolio.jpg';
 
 function Profile() {
     return (
         <>
-        
+        <HeaderIndex 
+        option1="Profile" 
+        link1 = "/"
+        logo = {pic}
+        option2="Edit Profile" 
+        link2 = "/edit-profile"
+        option3="Change Password" 
+        link3 = "/change-password"
+        option4="Settings"
+        link4 = "/settings" 
+        option5="Logout"
+        link5 = "/"/>
         </>
     )
 }
